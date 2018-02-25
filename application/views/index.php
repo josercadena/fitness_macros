e4069fd98000f37fe548f7c72fa8e262f6d2643d
jrodrigueznativapps <jrodriguez@nativapps.co>
2018-02-24 21:11:25 -0500

    Refactor reset

---
+++ application/views/index.php
@@ -0,0 +1,120 @@
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Carlos M. Arrieta - Professional Personal Trainer</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="css/full-slider.css" rel="stylesheet">
+
+  </head>
+
+  <body>
+
+    <!-- Navigation -->
+    <nav class="navbar navbar-expand-lg navbar-dark fixed-top">
+      <div class="container">
+        <a class="navbar-brand" href="#"></a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+          <ul class="navbar-nav ml-auto">
+            <li class="nav-item active">
+              <a class="nav-link" href="#">Home
+                <span class="sr-only">(current)</span>
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="login.php">WebApp</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="contact.php">Contacto</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+
+    <header>
+      <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+        <ol class="carousel-indicators">
+          <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+          <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+          <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+        </ol>
+        <div class="carousel-inner" role="listbox">
+          <!-- Slide One - Set the background image for this slide in the line below -->
+          <div class="carousel-item active" style="background-image: url('img/main/1.png')">
+            <div class="carousel-caption">
+              <h1 class="display-1">Alcanza tus metas</h1>
+              <p class="lead">Descubre una nueva versión de tí mismo</p>
+            </div>
+          </div>
+          <!-- Slide Two - Set the background image for this slide in the line below -->
+          <div class="carousel-item" style="background-image: url('img/main/2.png')">
+            <div class="carousel-caption">
+              <h1 class="display-1">Supérate cada día</h1>
+              <p class="lead">Porque cada paso cuenta</p>
+            </div>
+          </div>
+          <!-- Slide Three - Set the background image for this slide in the line below -->
+          <div class="carousel-item" style="background-image: url('img/main/3.png')">
+            <div class="carousel-caption">
+              <h1 class="display-1">Transforma tus hábitos</h1>
+              <p class="lead">Diseñamos un plan de alimentación exclusivo para ti</p>
+            </div>
+          </div>
+        </div>
+        <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="sr-only">Previous</span>
+        </a>
+        <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="sr-only">Next</span>
+        </a>
+      </div>
+    </header>
+
+    <!-- Page Content -->
+    <section class="py-5 bg-success">
+      <div class="container text-light">
+      <div class="row">
+
+        <div class="col">
+          <blockquote class="blockquote text-center">
+            <p class="mb-0">El mejor momento para iniciar un estilo de vida saludable fue hace unos años. El segundo mejor momento es <b>ahora</b>.</p>
+            <footer class="blockquote-footer text-light">Un sabio proverbio chino</footer>
+          </blockquote>
+          <center><a class="btn btn-light btn-lg font-weight-bold" href="contact.php">¡Comienza ahora!</a></center>
+        </div>
+
+      </div>
+      </div>
+    </section>
+
+    <!-- Footer -->
+    <footer class="py-5 bg-light">
+      <div class="container">
+        <p class="m-0 text-center text-secondary">Copyright &copy; <a class="text-secondary" href="http://twitter.com/josercadena"><b>Carlos M. Arrieta</b>, 2018</a></p>
+      </div>
+      <!-- /.container -->
+    </footer>
+
+    <!-- Bootstrap core JavaScript -->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+  </body>
+
+</html>
